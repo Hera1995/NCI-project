bb8f5d7d54370db2822cc0718ca5844792216395
Hera <heraleung1995@gmail.com>
2024-01-07 22:06:20 +0000

    review function has finished

--- src/main/java/com/nci/filmreview/controller/PageController.java
+++ src/main/java/com/nci/filmreview/controller/PageController.java
@@ -1,7 +1,6 @@
 package com.nci.filmreview.controller;
 
 import com.nci.filmreview.entity.Review;
-import com.nci.filmreview.entity.User;
 import com.nci.filmreview.service.UserService;
 import jakarta.annotation.Resource;
 import jakarta.servlet.http.HttpSession;
@@ -10,7 +9,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static com.nci.filmreview.controller.UserController.USER_KEY;
@@ -39,7 +37,7 @@ public class PageController {
     }
 
     @GetMapping("/detail")
-    public String movieDetail(@RequestParam String movieId, @RequestParam String title, @RequestParam String time, @RequestParam String imgUrl, Model model) {
+    public String movieDetail(@RequestParam String movieId, @RequestParam String title, @RequestParam String time, @RequestParam String imgUrl, Model model, HttpSession session) {
 
 
         //add reviews to model
@@ -48,15 +46,9 @@ public class PageController {
 
         //add users to model
         //create a new list to save userIds
-        List<User> users = new ArrayList<>();
 
-        //traverse all the reviews to get userIds
-        for (Review review : reviews) {
-            int userId = review.getUserId(); // get the userId of each Review object
-            User user = userService.findUser(userId); //find the user
-            users.add(user); // save the user to users list
-        }
-        model.addAttribute(USER_KEY, users);
+
+        model.addAttribute(USER_KEY, session.getAttribute(USER_KEY));
 
         //add movie info to model
         model.addAttribute("movieId", movieId);
