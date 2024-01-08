9afe67b55e274966193df08bbfde30b90d57e6f4
xiangnan.liu <x22190562@student.ncirl.ie>
2024-01-08 15:29:58 +0000

    bugs of delete and submit has been fixed

--- src/main/webapp/js/detail.js
+++ src/main/webapp/js/detail.js
@@ -76,60 +76,6 @@ $(document).ready(function () {
     });
 });
 
-
-// edit review
-/*$('#edit-btn').click(function () {
-
-
-    const review = {
-        content: $('#comment').val(),
-        imdbId: $('.detail-info:contains("imdbID")').text().trim().split(': ')[1]
-
-    }
-
-    console.log(review);
-
-    $.ajax({
-        type: 'POST',
-        url: window.location.origin + '/user/updateReview',
-        data: JSON.stringify(review),
-        dataType: 'json',
-        contentType: 'application/json',
-        success: function (response) {
-            if (response.status === 200) {
-                location.reload();
-            } else {
-                $('.error-alarm').html(response.msg);
-            }
-        },
-        error: function (response) {
-
-        }
-    });
-
-});*/
-
-// delete review
-/*$('#delete-btn').click(function () {
-    var reviewId = $(this).val(); // get reviewId
-    $.ajax({
-        type: 'GET',
-        url: window.location.origin + '/user/deleteReview',
-        data: {id: reviewId},
-        dataType: 'json',
-        success: function (response) {
-            console.log('Review deleted:', response);
-            location.reload();
-        },
-        error: function (xhr, status, error) {
-            console.error('Error deleting review:', error);
-        }
-
-    });
-
-
-});*/
-
 function hideURLbar() {
     window.scrollTo(0, 1);
 }
